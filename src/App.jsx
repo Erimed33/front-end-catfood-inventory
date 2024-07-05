6ca955106282b6104489c28ed724d7fbcbfe335f
Erimed33 <erikamedina@pursuit.org>
2024-07-05 08:57:21 -0400

    welcome-page-changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from '../NavBar'
 import Home from '../components/Home'
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
